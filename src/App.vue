fb207415a54d4c7f68493457d141209186b78cb0
Rémy Marronnier <rmarronnier@gmail.com>
2019-07-21 11:19:12 +0200

    Fixes menu + homepage + removes about page

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+            <router-link to="/breweries">List of breweries</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
+<style>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -18,12 +18,14 @@
 }
 #nav {
   padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
